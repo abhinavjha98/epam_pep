40cca58d111afab41e94ff788b750fda4fcc345a
Abhinav <abhinavjha98ald@gmail.com>
2019-03-05 21:01:54 +0530

    Added the first task of PEP

---
+++ epam_git_first/git-demo/src/main/java/com/epam/gitdemo/App.java
@@ -0,0 +1,13 @@
+package com.epam.gitdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
